2db452c90995d53e8127df285d23b21f3699c0de
aiupt <ulutas.alperen@outlook.com>
2025-01-27 02:46:20 +0300

    add count_characters func

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def main():
     path = "books/frankenstein.txt"
     file_content = read_file(path)
-    print(count_word(file_content))
+    print(count_character(file_content))
+    
 
 
 def read_file(path):
@@ -11,6 +12,17 @@ def read_file(path):
 def count_word(file_content):
     new_list = file_content.split()
     return len(new_list)
+
+def count_character(file_content):
+    new_string = file_content.lower()
+    new_dict = {}
+    for i in new_string:
+        if i not in new_dict:
+            new_dict[i]=1
+        else:
+            new_dict[i]+=1
+    return new_dict
+
 
 
 main()
