52836da68369667cdcc75b9e7909d485809a09ac
aiupt <ulutas.alperen@outlook.com>
2025-01-27 03:07:07 +0300

    a description of your changes here

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 def main():
     path = "books/frankenstein.txt"
     file_content = read_file(path)
-    print(count_character(file_content))
+    report_of_book(path,count_word(file_content),count_character(file_content))
     
 
 
 def read_file(path):
     with open(path)as f:
         return f.read()
+
+def report_of_book(name,words,characters):
+    print(f"--- Begin report of {name} ---")
+    print(f"{words} words found in the document\n")
+    for i in characters:
+        if i.isalpha() == 1:
+            print(f"The '{i}' character was found {characters[i]} times")
+    print("--- End report ---")
+
+
 
 def count_word(file_content):
     new_list = file_content.split()
